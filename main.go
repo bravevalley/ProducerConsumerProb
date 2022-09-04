f09981bb677de4579751fe9006ed19303a61f5f6
Dassyareg <dassyareg@gmail.com>
2022-09-04 23:33:01 +0100

    Done for the night

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
+	"sync"
 	"time"
-
-	"github/fatih/color"
 
 	"github.com/fatih/color"
 )
 
 // Number of product in a Batch
-const maxProduct int = 10
+// const maxProduct int = 10
 
 
-// Product Successfully Made, Product Failed, Total Product produced
-var sucProduct, failProduct, totalP int
+// // Product Successfully Made, Product Failed, Total Product produced
+// var sucProduct, failProduct, totalP int
 
 // Product Stat
 type orderStat struct {
@@ -30,18 +30,56 @@ type serviceHost struct {
 	quit chan chan error
 }
 
+// Sending Order to Factory
+func production(output *serviceHost) {
+	var orderNo int
+
+	produce(orderNo)
+
+}
+
+func produce(orderNumber int) {
+	// Increment the order Number
+
+	orderNumber++
+
+	// Take a moment to produce
+	processingTime := rand.Intn(7) * 1
+
+	// Inform client we have received order
+	color.White("We have recieved the Order #%d\nYour order will take %d seconds to be processed, kindly exercise patience", orderNumber, processingTime)
+
+
+	// Begin producing based on Probably
+	time.Sleep(time.Duration(processingTime)  * time.Second)
+
+	fmt.Println("I am still working...")
+
+	wg.Done()
+
+}
+
+var wg sync.WaitGroup
 
 func main() {
-
+	wg.Add(1)
 	// Seed to generate a random number
 	rand.Seed(time.Now().UnixNano())
 
 	// Print Starting text
 	color.Cyan("We are Open for business\nPlease send in your Orders....")
 
+	// Init Service host
+	factory := serviceHost{
+		order: make(chan orderStat),
+		quit: make(chan chan error),
+	}
+
 	// Produce
+	go production(&factory)
 
 	// Test Production
 
 	// Ending Test
+	wg.Wait()
 }
