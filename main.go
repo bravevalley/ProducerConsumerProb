974cf59e22819a21342295bae8aee2b7df4f1874
Dassyareg <dassyareg@gmail.com>
2022-09-05 22:03:51 +0100

    Completed the model but needs adjustments

--- main.go
+++ main.go
@@ -3,18 +3,16 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"sync"
 	"time"
 
 	"github.com/fatih/color"
 )
 
 // Number of product in a Batch
-// const maxProduct int = 10
+const maxProduct int = 10
 
-
-// // Product Successfully Made, Product Failed, Total Product produced
-// var sucProduct, failProduct, totalP int
+// Product Successfully Made, Product Failed, Total Product produced
+var sucProduct, failProduct int // totalP int
 
 // Product Stat
 type orderStat struct {
@@ -27,42 +25,93 @@ type orderStat struct {
 // between Main routine and Goroutine
 type serviceHost struct {
 	order chan orderStat
-	quit chan chan error
+	quit  chan chan error
+}
+
+// Stop production signal
+func (o *serviceHost) Close() error {
+	ch := make(chan error)
+	
+	o.quit <- ch
+	return <-ch
 }
 
 // Sending Order to Factory
-func production(output *serviceHost) {
+func production(incomingOrder *serviceHost) {
+
+	// Create a variable to keep track of the incoming order
 	var orderNo int
 
-	produce(orderNo)
+	// Send the incoming order to the factory for procuction
+	// We get the output of the production
+	for {
+
+		output := produce(orderNo)
+
+		select {
+
+		// Send the product back to client(MAIN FUNC) via a channel
+		case incomingOrder.order <- *output:
+
+		case quitCh := <-incomingOrder.quit:
+			close(incomingOrder.order)
+			close(quitCh)
+			return
+		}
+	}
 
 }
 
-func produce(orderNumber int) {
-	// Increment the order Number
+func produce(orderNumber int) *orderStat {
 
+	var exitMesg string
+	success := false
+
+	// Increment the order Number
 	orderNumber++
 
 	// Take a moment to produce
-	processingTime := rand.Intn(7) * 1
+	processingTime := rand.Intn(7) + 1
 
 	// Inform client we have received order
 	color.White("We have recieved the Order #%d\nYour order will take %d seconds to be processed, kindly exercise patience", orderNumber, processingTime)
 
-
 	// Begin producing based on Probably
-	time.Sleep(time.Duration(processingTime)  * time.Second)
+	time.Sleep(time.Duration(processingTime) * time.Second)
 
-	fmt.Println("I am still working...")
+	temp := rand.Intn(12) + 1
 
-	wg.Done()
+	switch {
+	case temp < 2:
+		failProduct++
+		exitMesg = fmt.Sprintln("Apologies, we are unable to produce as the temperature is too low for the machines to work")
+	case temp < 5:
+		failProduct++
+		exitMesg = fmt.Sprintln("We are experience some idle time here.")
+	case temp < 9:
+		sucProduct++
+		exitMesg = fmt.Sprintln("Successfully made the product.")
+		success = true
+	case temp <= 12:
+		failProduct++
+		exitMesg = fmt.Sprintln("Too hot for worker to work.")
 
+	}
+
+	p := orderStat{
+		batchNo: orderNumber,
+		Memo:    exitMesg,
+		suc:     success,
+	}
+
+	return &p
 }
 
-var wg sync.WaitGroup
+// Function to call when the temperature is too low to work
+//accepts an argument of the orderNo
 
 func main() {
-	wg.Add(1)
+
 	// Seed to generate a random number
 	rand.Seed(time.Now().UnixNano())
 
@@ -72,14 +121,31 @@ func main() {
 	// Init Service host
 	factory := serviceHost{
 		order: make(chan orderStat),
-		quit: make(chan chan error),
+		quit:  make(chan chan error),
 	}
 
 	// Produce
 	go production(&factory)
 
 	// Test Production
+	for i := range factory.order {
+		if i.batchNo <= maxProduct {
+			if i.suc {
+				color.Green(i.Memo)
+				color.Green("Order #%d was produced successfully", i.batchNo)
+			} else {
+				color.Red(i.Memo)
+				color.Red("What a terrible day")
+			}
+		} else {
+			color.Cyan("Done making pizzas...")
+			err := factory.Close()
+			if err != nil {
+				color.Red("*** Error closing channel!", err)
+			}
+		}
+	}
 
 	// Ending Test
-	wg.Wait()
+
 }
